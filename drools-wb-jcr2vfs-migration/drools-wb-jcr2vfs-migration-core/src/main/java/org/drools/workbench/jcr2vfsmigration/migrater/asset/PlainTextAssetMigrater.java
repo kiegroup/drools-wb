b633aa7ef12690b2d6e1312a92f535a7bf879589
Petr Siroky <psiroky@redhat.com>
2013-09-11 13:36:24 +0200

    [BZ1004869] jcr2vfs: fix comments (# -> //) migration
     * end of lines were thrown away, thats fixed now
     * comments _not_ starting at the start of line are migrated too
       e.g. 'some text # comment' 'some text // comment'
     * hash tags inside double and single qoutes are not touched
     * DSL debug mark '#/' is not touched
     * escaped quotes (\' and \") are ignored

--- drools-wb-jcr2vfs-migration/drools-wb-jcr2vfs-migration-core/src/main/java/org/drools/workbench/jcr2vfsmigration/migrater/asset/PlainTextAssetMigrater.java
+++ drools-wb-jcr2vfs-migration/drools-wb-jcr2vfs-migration-core/src/main/java/org/drools/workbench/jcr2vfsmigration/migrater/asset/PlainTextAssetMigrater.java
@@ -1,8 +1,5 @@
 package org.drools.workbench.jcr2vfsmigration.migrater.asset;
 
-import java.util.Map;
-import java.util.Scanner;
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -11,6 +8,7 @@ import org.drools.guvnor.client.common.AssetFormats;
 import org.drools.guvnor.client.rpc.Module;
 import org.drools.guvnor.server.RepositoryAssetService;
 import org.drools.repository.AssetItem;
+import org.drools.workbench.jcr2vfsmigration.migrater.util.DRLMigrationUtils;
 import org.drools.workbench.jcr2vfsmigration.migrater.util.MigrationPathManager;
 import org.kie.commons.io.IOService;
 import org.kie.commons.java.nio.base.options.CommentedOption;
@@ -50,26 +48,13 @@ public class PlainTextAssetMigrater extends BaseAssetMigrater {
         String content = jcrAssetItem.getContent();
 
 
-        //Support for # has been removed from Drools Expert
+        // Support for # has been removed from Drools Expert
         if (AssetFormats.DSL.equals(jcrAssetItem.getFormat())
-                || AssetFormats.DSL_TEMPLATE_RULE.equals(jcrAssetItem.getFormat())                
-        		|| AssetFormats.RULE_TEMPLATE.equals(jcrAssetItem.getFormat())
-        		|| AssetFormats.DRL.equals(jcrAssetItem.getFormat())
+                || AssetFormats.DSL_TEMPLATE_RULE.equals(jcrAssetItem.getFormat())
+                || AssetFormats.RULE_TEMPLATE.equals(jcrAssetItem.getFormat())
+                || AssetFormats.DRL.equals(jcrAssetItem.getFormat())
                 || AssetFormats.FUNCTION.equals(jcrAssetItem.getFormat())) {
-        	StringBuffer sb = new StringBuffer();
-            Scanner scanner = new Scanner(content);
-            
-            while (scanner.hasNextLine()) {
-            	  String line = scanner.nextLine();
-            	  if(line.startsWith("#")) {
-            		  sb.append(line.replaceFirst("#", "//"));
-            	  } else {
-            		  sb.append(line);
-            	  }            	  
-            }
-            
-            scanner.close();
-            content = sb.toString();
+            content = DRLMigrationUtils.migrateStartOfCommentChar(content);
         }
 
         ioService.write( nioPath,
