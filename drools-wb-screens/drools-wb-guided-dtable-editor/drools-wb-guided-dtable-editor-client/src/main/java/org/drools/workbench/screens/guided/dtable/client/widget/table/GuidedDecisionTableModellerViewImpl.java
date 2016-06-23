apache/incubator-kie-tools

commit f186c21226c8d612d3faf998df4744a28dca5d8c

GUVNOR-2559: Guided Decision Table Editor: Improve appearance (for Red Hat Summit) - Some colour changes. (#210)

drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-client/src/main/java/org/drools/workbench/screens/guided/dtable/client/widget/table/GuidedDecisionTableModellerViewImpl.java
@@ -61,7 +61,6 @@ import org.uberfire.ext.widgets.common.client.common.popups.FormStylePopup;
 import org.uberfire.ext.widgets.common.client.common.popups.footers.ModalFooterOKCancelButtons;
 import org.uberfire.ext.wires.core.grids.client.model.Bounds;
 import org.uberfire.ext.wires.core.grids.client.model.GridColumn;
-import org.uberfire.ext.wires.core.grids.client.model.impl.BaseBounds;
 import org.uberfire.ext.wires.core.grids.client.widget.grid.GridWidget;
 import org.uberfire.ext.wires.core.grids.client.widget.layer.GridLayer;
 import org.uberfire.ext.wires.core.grids.client.widget.layer.impl.DefaultGridLayer;
@@ -74,18 +73,6 @@ import org.uberfire.mvp.ParameterizedCommand;
 public class GuidedDecisionTableModellerViewImpl extends Composite implements GuidedDecisionTableModellerView {
 
     private static final double VP_SCALE = 1.0;
-
-    private final double BOUNDS_MIN_X = -2000;
-    private final double BOUNDS_MAX_X = 2000;
-    private final double BOUNDS_MIN_Y = -2000;
-    private final double BOUNDS_MAX_Y = 2000;
-
-    private final double BOUNDS_PADDING = 20;
-
-    private final Bounds bounds = new BaseBounds( BOUNDS_MIN_X,
-                                                  BOUNDS_MIN_Y,
-                                                  BOUNDS_MAX_X - BOUNDS_MIN_X,
-                                                  BOUNDS_MAX_Y - BOUNDS_MIN_Y );
 
     interface GuidedDecisionTableModellerViewImplUiBinder extends UiBinder<Widget, GuidedDecisionTableModellerViewImpl> {
 
@@ -200,6 +187,7 @@ public class GuidedDecisionTableModellerViewImpl extends Composite implements Gu
     private VerticalPanel actionsConfigWidget;
 
     private final RuleSelector ruleSelector = new RuleSelector();
+    private final GuidedDecisionTableModellerBoundsHelper boundsHelper = new GuidedDecisionTableModellerBoundsHelper();
 
     public GuidedDecisionTableModellerViewImpl() {
         initWidget( uiBinder.createAndBindUi( this ) );
@@ -315,8 +303,8 @@ public class GuidedDecisionTableModellerViewImpl extends Composite implements Gu
         //Ensure the first Decision Table is visible
         if ( gridLayer.getGridWidgets().isEmpty() ) {
             final Transform t = gridLayer.getViewport().getTransform();
-            t.translate( BOUNDS_PADDING - gridWidget.getX(),
-                         BOUNDS_PADDING - gridWidget.getY() );
+            t.translate( GuidedDecisionTableModellerBoundsHelper.BOUNDS_PADDING - gridWidget.getX(),
+                         GuidedDecisionTableModellerBoundsHelper.BOUNDS_PADDING - gridWidget.getY() );
         }
         gridLayer.add( gridWidget );
         gridLayer.batch();
@@ -970,30 +958,7 @@ public class GuidedDecisionTableModellerViewImpl extends Composite implements Gu
 
     @Override
     public Bounds getBounds() {
-        double minX = BOUNDS_MIN_X;
-        double minY = BOUNDS_MIN_Y;
-        double maxX = BOUNDS_MAX_X;
-        double maxY = BOUNDS_MAX_Y;
-
-        if ( presenter != null ) {
-            for ( GuidedDecisionTableView.Presenter dtPresenter : presenter.getAvailableDecisionTables() ) {
-                final GuidedDecisionTableView dtView = dtPresenter.getView();
-                minX = Math.min( dtView.getX() - BOUNDS_PADDING,
-                                 BOUNDS_MIN_X );
-                minY = Math.min( dtView.getY() - BOUNDS_PADDING,
-                                 BOUNDS_MIN_Y );
-                maxX = Math.max( dtView.getX() + dtView.getWidth() + BOUNDS_PADDING,
-                                 BOUNDS_MAX_X );
-                maxY = Math.max( dtView.getY() + dtView.getHeight() + BOUNDS_PADDING,
-                                 BOUNDS_MAX_Y );
-            }
-        }
-        bounds.setX( minX );
-        bounds.setY( minY );
-        bounds.setWidth( maxX - minX );
-        bounds.setHeight( maxY - minY );
-
-        return bounds;
+        return boundsHelper.getBounds( presenter.getAvailableDecisionTables() );
     }
 
     @Override

drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-client/src/main/java/org/drools/workbench/screens/guided/dtable/client/editor/menu/RadarMenuViewImpl.java
@@ -124,7 +124,6 @@ public class RadarMenuViewImpl extends Composite implements RadarMenuView {
                                                visibleBounds.getY() );
             }
         } );
-        visibleBounds.setStrokeColor( ColorName.LIGHTGRAY );
         visibleBounds.setFillColor( ColorName.GRAY );
         visibleBounds.setAlpha( 0.25 );
         visibleBounds.setLocation( new Point2D( ( RADAR_WIDTH - visibleBounds.getWidth() ) / 2,
@@ -203,7 +202,7 @@ public class RadarMenuViewImpl extends Composite implements RadarMenuView {
         final Group g = new Group();
         final Rectangle r = new Rectangle( view.getWidth(),
                                            view.getHeight() );
-        r.setFillColor( view.getRenderer().getTheme().getBodyBackground( view.getModel().getColumns().get( 0 ) ).getFillColor() );
+        r.setFillColor( ColorName.LIGHTGRAY );
         r.setLocation( view.getLocation() );
         g.setListening( false );
         g.add( r );

drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-client/src/main/java/org/drools/workbench/screens/guided/dtable/client/widget/table/GuidedDecisionTableModellerBoundsHelper.java
@@ -1,0 +1,72 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.workbench.screens.guided.dtable.client.widget.table;
+
+import java.util.Set;
+
+import org.uberfire.ext.wires.core.grids.client.model.Bounds;
+import org.uberfire.ext.wires.core.grids.client.model.impl.BaseBounds;
+
+/**
+ * Helper class to calculate the maximum bounds enclosed by Decision Tables.
+ */
+public class GuidedDecisionTableModellerBoundsHelper {
+
+    public static final double BOUNDS_MIN_X = -2000;
+    public static final double BOUNDS_MAX_X = 2000;
+    public static final double BOUNDS_MIN_Y = -2000;
+    public static final double BOUNDS_MAX_Y = 2000;
+
+    public static final double BOUNDS_PADDING = 20;
+
+    private final Bounds bounds = new BaseBounds( BOUNDS_MIN_X,
+                                                  BOUNDS_MIN_Y,
+                                                  BOUNDS_MAX_X - BOUNDS_MIN_X,
+                                                  BOUNDS_MAX_Y - BOUNDS_MIN_Y );
+
+    /**
+     * Returns the maximum bounds (subject to a minimum) enclosed by the provided collection of Decision Tables.
+     * @param dtPresenters The collection of Decision Tables to check. Cannot be null. Can be empty, in which case the minimum bounds is returned.
+     * @return The maximum bounds enclosed by the Decision Tables.
+     */
+    public Bounds getBounds( final Set<GuidedDecisionTableView.Presenter> dtPresenters ) {
+        double minX = BOUNDS_MIN_X;
+        double minY = BOUNDS_MIN_Y;
+        double maxX = BOUNDS_MAX_X;
+        double maxY = BOUNDS_MAX_Y;
+
+        for ( GuidedDecisionTableView.Presenter dtPresenter : dtPresenters ) {
+            final GuidedDecisionTableView dtView = dtPresenter.getView();
+            minX = Math.min( dtView.getX() - BOUNDS_PADDING,
+                             minX );
+            minY = Math.min( dtView.getY() - BOUNDS_PADDING,
+                             minY );
+            maxX = Math.max( dtView.getX() + dtView.getWidth() + BOUNDS_PADDING,
+                             maxX );
+            maxY = Math.max( dtView.getY() + dtView.getHeight() + BOUNDS_PADDING,
+                             maxY );
+        }
+
+        bounds.setX( minX );
+        bounds.setY( minY );
+        bounds.setWidth( maxX - minX );
+        bounds.setHeight( maxY - minY );
+
+        return bounds;
+    }
+
+}

drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-client/src/main/java/org/drools/workbench/screens/guided/dtable/client/widget/table/GuidedDecisionTableViewImpl.java
@@ -129,7 +129,7 @@ public class GuidedDecisionTableViewImpl extends BaseGridWidget implements Guide
                 .L( 0.5,
                     HEADER_CAPTION_HEIGHT + 0.5 );
 
-        final Text caption = theme.getBodyText()
+        final Text caption = theme.getHeaderText()
                 .setText( model.getTableName() )
                 .setX( HEADER_CAPTION_WIDTH / 2 )
                 .setY( HEADER_CAPTION_HEIGHT / 2 );
@@ -187,11 +187,18 @@ public class GuidedDecisionTableViewImpl extends BaseGridWidget implements Guide
         final BaseGridRendererHelper.RenderingBlockInformation floatingBlockInformation = renderingInformation.getFloatingBlockInformation();
         if ( !floatingBlockInformation.getColumns().isEmpty() ) {
             headerCaption.setX( floatingBlockInformation.getX() );
+
         } else {
             headerCaption.setX( 0.0 );
         }
 
         add( headerCaption );
+    }
+
+    @Override
+    public void select() {
+        presenter.select( this );
+        super.select();
     }
 
     @Override

drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-client/src/main/java/org/drools/workbench/screens/guided/dtable/client/widget/table/themes/GuidedDecisionTableTheme.java
@@ -53,9 +53,9 @@ public class GuidedDecisionTableTheme implements GridRendererTheme {
 
     private static final int GRID_FONT_SIZE = 12;
 
-    private static final String ROW_NUMBER_COLUMN_BACKGROUND_COLOUR = Color.rgbToBrowserHexColor( 246, 246, 246 );
+    private static final String ROW_NUMBER_COLUMN_BACKGROUND_COLOUR = Color.rgbToBrowserHexColor( 255, 255, 255 );
 
-    private static final String DESCRIPTION_COLUMN_BACKGROUND_COLOUR = Color.rgbToBrowserHexColor( 246, 246, 246 );
+    private static final String DESCRIPTION_COLUMN_BACKGROUND_COLOUR = Color.rgbToBrowserHexColor( 255, 255, 255 );
 
     private static final String METADATA_COLUMN_BACKGROUND_COLOUR = Color.rgbToBrowserHexColor( 255, 255, 224 );
 

drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-client/src/test/java/org/drools/workbench/screens/guided/dtable/client/widget/table/GuidedDecisionTableModellerBoundsHelperTest.java
@@ -1,0 +1,183 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.drools.workbench.screens.guided.dtable.client.widget.table;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.uberfire.ext.wires.core.grids.client.model.Bounds;
+
+import static org.drools.workbench.screens.guided.dtable.client.widget.table.GuidedDecisionTableModellerBoundsHelper.*;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GuidedDecisionTableModellerBoundsHelperTest {
+
+    private GuidedDecisionTableModellerBoundsHelper helper = new GuidedDecisionTableModellerBoundsHelper();
+
+    @Test(expected = NullPointerException.class)
+    public void nullCollection() {
+        helper.getBounds( null );
+    }
+
+    @Test
+    public void emptyCollection() {
+        final Bounds b = helper.getBounds( Collections.<GuidedDecisionTableView.Presenter>emptySet() );
+        assertEquals( BOUNDS_MIN_X,
+                      b.getX(),
+                      0.0 );
+        assertEquals( BOUNDS_MIN_Y,
+                      b.getY(),
+                      0.0 );
+        assertEquals( BOUNDS_MAX_X - BOUNDS_MIN_X,
+                      b.getWidth(),
+                      0.0 );
+        assertEquals( BOUNDS_MAX_Y - BOUNDS_MIN_Y,
+                      b.getHeight(),
+                      0.0 );
+    }
+
+    @Test
+    public void emptyOneDecisionTableWithinMinimumBounds() {
+        final Bounds b = helper.getBounds( new HashSet<GuidedDecisionTableView.Presenter>() {{
+            add( makeTable( 0, 0, 400, 400 ) );
+        }} );
+        assertEquals( BOUNDS_MIN_X,
+                      b.getX(),
+                      0.0 );
+        assertEquals( BOUNDS_MIN_Y,
+                      b.getY(),
+                      0.0 );
+        assertEquals( BOUNDS_MAX_X - BOUNDS_MIN_X,
+                      b.getWidth(),
+                      0.0 );
+        assertEquals( BOUNDS_MAX_Y - BOUNDS_MIN_Y,
+                      b.getHeight(),
+                      0.0 );
+    }
+
+    @Test
+    public void multipleDecisionTablesCheckLeftBounds() {
+        final Bounds b = helper.getBounds( new HashSet<GuidedDecisionTableView.Presenter>() {{
+            add( makeTable( 0, 0, 400, 400 ) );
+            add( makeTable( -2200, 0, 400, 400 ) );
+        }} );
+
+        final double expectedMinX = -2200 - BOUNDS_PADDING;
+
+        assertEquals( expectedMinX,
+                      b.getX(),
+                      0.0 );
+        assertEquals( BOUNDS_MIN_Y,
+                      b.getY(),
+                      0.0 );
+        assertEquals( BOUNDS_MAX_X - expectedMinX,
+                      b.getWidth(),
+                      0.0 );
+        assertEquals( BOUNDS_MAX_Y - BOUNDS_MIN_Y,
+                      b.getHeight(),
+                      0.0 );
+    }
+
+    @Test
+    public void multipleDecisionTablesCheckRightBounds() {
+        final Bounds b = helper.getBounds( new HashSet<GuidedDecisionTableView.Presenter>() {{
+            add( makeTable( 0, 0, 400, 400 ) );
+            add( makeTable( 1800, 0, 400, 400 ) );
+        }} );
+
+        final double expectedMaxX = 1800 + 400 + BOUNDS_PADDING;
+
+        assertEquals( BOUNDS_MIN_X,
+                      b.getX(),
+                      0.0 );
+        assertEquals( BOUNDS_MIN_Y,
+                      b.getY(),
+                      0.0 );
+        assertEquals( expectedMaxX - BOUNDS_MIN_X,
+                      b.getWidth(),
+                      0.0 );
+        assertEquals( BOUNDS_MAX_Y - BOUNDS_MIN_Y,
+                      b.getHeight(),
+                      0.0 );
+    }
+
+    @Test
+    public void multipleDecisionTablesCheckTopBounds() {
+        final Bounds b = helper.getBounds( new HashSet<GuidedDecisionTableView.Presenter>() {{
+            add( makeTable( 0, 0, 400, 400 ) );
+            add( makeTable( 0, -2200, 400, 400 ) );
+        }} );
+
+        final double expectedMinY = -2200 - BOUNDS_PADDING;
+
+        assertEquals( BOUNDS_MIN_X,
+                      b.getX(),
+                      0.0 );
+        assertEquals( expectedMinY,
+                      b.getY(),
+                      0.0 );
+        assertEquals( BOUNDS_MAX_X - BOUNDS_MIN_X,
+                      b.getWidth(),
+                      0.0 );
+        assertEquals( BOUNDS_MAX_Y - expectedMinY,
+                      b.getHeight(),
+                      0.0 );
+    }
+
+    @Test
+    public void multipleDecisionTablesCheckBottomBounds() {
+        final Bounds b = helper.getBounds( new HashSet<GuidedDecisionTableView.Presenter>() {{
+            add( makeTable( 0, 0, 400, 400 ) );
+            add( makeTable( 0, 1800, 400, 400 ) );
+        }} );
+
+        final double expectedMaxY = 1800 + 400 + BOUNDS_PADDING;
+
+        assertEquals( BOUNDS_MIN_X,
+                      b.getX(),
+                      0.0 );
+        assertEquals( BOUNDS_MIN_Y,
+                      b.getY(),
+                      0.0 );
+        assertEquals( BOUNDS_MAX_X - BOUNDS_MIN_X,
+                      b.getWidth(),
+                      0.0 );
+        assertEquals( expectedMaxY - BOUNDS_MIN_Y,
+                      b.getHeight(),
+                      0.0 );
+    }
+
+    private GuidedDecisionTableView.Presenter makeTable( final double x,
+                                                         final double y,
+                                                         final double width,
+                                                         final double height ) {
+        final GuidedDecisionTableView.Presenter dtPresenter = mock( GuidedDecisionTableView.Presenter.class );
+        final GuidedDecisionTableView dtView = mock( GuidedDecisionTableView.class );
+        when( dtPresenter.getView() ).thenReturn( dtView );
+        when( dtView.getX() ).thenReturn( x );
+        when( dtView.getY() ).thenReturn( y );
+        when( dtView.getWidth() ).thenReturn( width );
+        when( dtView.getHeight() ).thenReturn( height );
+        return dtPresenter;
+    }
+
+}

kiegroup/drools-wb

commit 750905c9033bd4be16c2a4ef6dbedd2149d3dd1c

GUVNOR-2559: Guided Decision Table Editor: Improve appearance (for Red Hat Summit) - Fix regression introduced by f186c21226c8d612d3faf998df4744a28dca5d8c

drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-client/src/main/java/org/drools/workbench/screens/guided/dtable/client/widget/table/GuidedDecisionTableModellerViewImpl.java
@@ -1,6 +1,7 @@
 package org.drools.workbench.screens.guided.dtable.client.widget.table;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import javax.annotation.PostConstruct;
@@ -958,7 +959,11 @@ public class GuidedDecisionTableModellerViewImpl extends Composite implements Gu
 
     @Override
     public Bounds getBounds() {
-        return boundsHelper.getBounds( presenter.getAvailableDecisionTables() );
+        if ( presenter == null ) {
+            return boundsHelper.getBounds( Collections.emptySet() );
+        } else {
+            return boundsHelper.getBounds( presenter.getAvailableDecisionTables() );
+        }
     }
 
     @Override