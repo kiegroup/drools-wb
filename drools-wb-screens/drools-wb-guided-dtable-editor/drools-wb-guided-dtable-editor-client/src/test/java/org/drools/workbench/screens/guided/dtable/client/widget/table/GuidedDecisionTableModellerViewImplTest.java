2f175b77dcfe7a75b1afad0753e05eb5d924e1a1
Guilherme Carreiro <karreiro@gmail.com>
2017-09-14 18:04:32 -0300

    GUVNOR-3468: GridLienzoPanel/GridLienzoScrollUI: Improve re-usability

--- drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-client/src/test/java/org/drools/workbench/screens/guided/dtable/client/widget/table/GuidedDecisionTableModellerViewImplTest.java
+++ drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-client/src/test/java/org/drools/workbench/screens/guided/dtable/client/widget/table/GuidedDecisionTableModellerViewImplTest.java
@@ -356,7 +356,7 @@ public class GuidedDecisionTableModellerViewImplTest {
         doReturn(mediators).when(viewport).getMediators();
 
         doReturn(element).when(mockGridPanel).getElement();
-        doReturn(mainPanel).when(mockGridPanel).getMainPanel();
+        doReturn(mainPanel).when(mockGridPanel).getScrollPanel();
         doReturn(viewport).when(mockGridPanel).getViewport();
 
         view.setup();
@@ -384,7 +384,7 @@ public class GuidedDecisionTableModellerViewImplTest {
         doReturn(mediators).when(viewport).getMediators();
 
         doReturn(element).when(mockGridPanel).getElement();
-        doReturn(mainPanel).when(mockGridPanel).getMainPanel();
+        doReturn(mainPanel).when(mockGridPanel).getScrollPanel();
         doReturn(viewport).when(mockGridPanel).getViewport();
 
         view.setupGridPanel();
