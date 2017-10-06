dac5bd27426f17753bca1231ddbd65ecb40c4718
Guilherme Carreiro <karreiro@gmail.com>
2017-10-06 14:22:21 -0300

    GUVNOR-3200: [Guided Decision Table] Access control to prohibit business users from modifying columns (#644)

--- drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-client/src/main/java/org/drools/workbench/screens/guided/dtable/client/widget/table/GuidedDecisionTableModellerView.java
+++ drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-client/src/main/java/org/drools/workbench/screens/guided/dtable/client/widget/table/GuidedDecisionTableModellerView.java
@@ -58,6 +58,10 @@ public interface GuidedDecisionTableModellerView extends UberView<GuidedDecision
                                                          ViewMenuBuilder.SupportsZoom,
                                                          InsertMenuBuilder.SupportsInsertColumn {
 
+    void enableColumnOperationsMenu();
+
+    void disableColumnOperationsMenu();
+
     void clear();
 
     void addDecisionTable(final GuidedDecisionTableView gridWidget);
@@ -146,5 +150,7 @@ public interface GuidedDecisionTableModellerView extends UberView<GuidedDecision
         void updateLinks();
 
         void openNewGuidedDecisionTableColumnWizard();
+
+        boolean isColumnCreationEnabledToActiveDecisionTable();
     }
 }
