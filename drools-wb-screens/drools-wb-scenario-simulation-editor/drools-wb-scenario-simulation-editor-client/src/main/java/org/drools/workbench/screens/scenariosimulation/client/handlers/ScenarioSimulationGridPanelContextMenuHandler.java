18a9dd72964a1a18076b025fb8c4aa4f2af2488c
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2018-08-07 09:57:38 +0200

    [DROOLS-2844] Preventing more then one contextual menu to be shown in the same time

--- drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/main/java/org/drools/workbench/screens/scenariosimulation/client/handlers/ScenarioSimulationGridPanelContextMenuHandler.java
+++ drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/main/java/org/drools/workbench/screens/scenariosimulation/client/handlers/ScenarioSimulationGridPanelContextMenuHandler.java
@@ -15,28 +15,37 @@
  */
 package org.drools.workbench.screens.scenariosimulation.client.handlers;
 
+import com.ait.lienzo.client.core.types.Point2D;
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NativeEvent;
 import com.google.gwt.event.dom.client.ContextMenuEvent;
 import com.google.gwt.event.dom.client.ContextMenuHandler;
-import com.google.gwt.event.shared.GwtEvent;
-import com.google.gwt.event.shared.HandlerManager;
-import com.google.gwt.event.shared.HasHandlers;
-import org.drools.workbench.screens.scenariosimulation.client.widgets.ScenarioGridLayer;
-import org.drools.workbench.screens.scenariosimulation.client.widgets.ScenarioGridPanel;
+import org.drools.workbench.screens.scenariosimulation.client.editor.menu.GridContextMenu;
+import org.drools.workbench.screens.scenariosimulation.client.editor.menu.HeaderContextMenu;
+import org.drools.workbench.screens.scenariosimulation.client.metadata.ScenarioHeaderMetaData;
+import org.drools.workbench.screens.scenariosimulation.client.widgets.ScenarioGrid;
+import org.uberfire.ext.wires.core.grids.client.util.CoordinateUtilities;
 
-public class ScenarioSimulationGridPanelContextMenuHandler implements ContextMenuHandler,
-                                                                      HasHandlers {
+import static org.drools.workbench.screens.scenariosimulation.client.utils.ScenarioSimulationGridHeaderUtilities.getColumnScenarioHeaderMetaData;
 
-    private ScenarioGridPanel scenarioGridPanel;
-    private ScenarioGridLayer scenarioGridLayer;
-    private HandlerManager handlerManager;
+public class ScenarioSimulationGridPanelContextMenuHandler implements ContextMenuHandler {
 
-    public ScenarioSimulationGridPanelContextMenuHandler(final ScenarioGridLayer scenarioGridLayer) {
-        this.scenarioGridLayer = scenarioGridLayer;
-        handlerManager = new HandlerManager(this);
+    private ScenarioGrid scenarioGrid;
+    private GridContextMenu gridContextMenu;
+    private HeaderContextMenu headerContextMenu;
+
+
+    public ScenarioSimulationGridPanelContextMenuHandler(final ScenarioGrid scenarioGrid) {  // This could be make more generic, but apparently DefaultGridLayer could have more then one grid, so we have to find a way to
+        // detect and identify the click on each of them
+        this.scenarioGrid = scenarioGrid;
     }
 
-    public void setScenarioGridPanel(ScenarioGridPanel scenarioGridPanel) {
-        this.scenarioGridPanel = scenarioGridPanel;
+    public void setGridContextMenu(GridContextMenu gridContextMenu) {
+        this.gridContextMenu = gridContextMenu;
+    }
+
+    public void setHeaderContextMenu(HeaderContextMenu headerContextMenu) {
+        this.headerContextMenu = headerContextMenu;
     }
 
 
@@ -45,11 +54,75 @@ public class ScenarioSimulationGridPanelContextMenuHandler implements ContextMen
     public void onContextMenu(final ContextMenuEvent event) {
         event.preventDefault();
         event.stopPropagation();
+        final int canvasX = getRelativeX(event);
+        final int canvasY = getRelativeY(event);
+        final boolean isShiftKeyDown = event.getNativeEvent().getShiftKey();
+        final boolean isControlKeyDown = event.getNativeEvent().getCtrlKey();
+        final Point2D ap = CoordinateUtilities.convertDOMToGridCoordinate(scenarioGrid,
+                                                                          new Point2D(canvasX,
+                                                                                      canvasY));
+        final Integer uiColumnIndex = CoordinateUtilities.getUiColumnIndex(scenarioGrid,
+                                                                           ap.getX());
+        if (uiColumnIndex == null) {
+            return;
+        }
+        gridContextMenu.hide();
+        headerContextMenu.hide();
+        if (!manageHeaderClick(scenarioGrid, event.getNativeEvent().getClientX(), event.getNativeEvent().getClientY(), ap.getY(), uiColumnIndex)) {
+            manageBodyClick(scenarioGrid, event.getNativeEvent().getClientX(), event.getNativeEvent().getClientY(), ap.getY(), uiColumnIndex, isShiftKeyDown, isControlKeyDown);
+        }
     }
 
-    @Override
-    public void fireEvent(GwtEvent<?> event) {
-        handlerManager.fireEvent(event);
+    /**
+     * This method check if the click happened on an <b>header</b> cell. If it is so, manage it and returns <code>true</code>,
+     * otherwise returns <code>false</code>
+     * @param scenarioGrid
+     * @param left
+     * @param top
+     * @param gridY
+     * @param uiColumnIndex
+     * @return
+     */
+    private boolean manageHeaderClick(ScenarioGrid scenarioGrid, int left, int top, double gridY, Integer uiColumnIndex) {
+        ScenarioHeaderMetaData columnMetadata = getColumnScenarioHeaderMetaData(scenarioGrid, scenarioGrid.getModel().getColumns().get(uiColumnIndex), gridY);
+        if (columnMetadata == null) {
+            return false;
+        }
+        headerContextMenu.show(left, top);
+        return true;
     }
 
+    /**
+     * This method check if the click happened on an <b>body</b> cell. If it is so, manage it and returns <code>true</code>,
+     * otherwise returns <code>false</code>
+     * @param scenarioGrid
+     * @param left
+     * @param top
+     * @param gridY
+     * @param uiColumnIndex
+     * @param isShiftKeyDown
+     * @param isControlKeyDown
+     * @return
+     */
+    private boolean manageBodyClick(ScenarioGrid scenarioGrid, int left, int top, double gridY, Integer uiColumnIndex, boolean isShiftKeyDown, boolean isControlKeyDown) {
+        scenarioGrid.deselect();
+        final Integer uiRowIndex = CoordinateUtilities.getUiRowIndex(scenarioGrid, gridY);
+        if (uiRowIndex == null) {
+            return false;
+        }
+        gridContextMenu.show(left, top);
+        return true;
+    }
+
+    private int getRelativeX(final ContextMenuEvent event) {
+        final NativeEvent e = event.getNativeEvent();
+        final Element target = event.getRelativeElement();
+        return e.getClientX() - target.getAbsoluteLeft() + target.getScrollLeft() + target.getOwnerDocument().getScrollLeft();
+    }
+
+    private int getRelativeY(final ContextMenuEvent event) {
+        final NativeEvent e = event.getNativeEvent();
+        final Element target = event.getRelativeElement();
+        return e.getClientY() - target.getAbsoluteTop() + target.getScrollTop() + target.getOwnerDocument().getScrollTop();
+    }
 }
