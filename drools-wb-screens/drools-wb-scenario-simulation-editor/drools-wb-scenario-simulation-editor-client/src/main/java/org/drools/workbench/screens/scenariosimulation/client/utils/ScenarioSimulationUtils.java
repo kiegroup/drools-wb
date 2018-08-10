8f3b799cb96792dcc811781edd6ee8a8e8706b11
Gabriele Cardosi <gabriele.cardosi@gmail.com>
2018-08-10 10:46:08 +0200

    [DROOLS-2860] Grouping column headers by column group. Implemented TriFunction. Add required tests

--- drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/main/java/org/drools/workbench/screens/scenariosimulation/client/utils/ScenarioSimulationUtils.java
+++ drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/main/java/org/drools/workbench/screens/scenariosimulation/client/utils/ScenarioSimulationUtils.java
@@ -15,18 +15,34 @@
  */
 package org.drools.workbench.screens.scenariosimulation.client.utils;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.drools.workbench.screens.scenariosimulation.client.factories.FactoryProvider;
+import org.drools.workbench.screens.scenariosimulation.client.interfaces.TriFunction;
 import org.drools.workbench.screens.scenariosimulation.client.metadata.ScenarioHeaderMetaData;
 import org.drools.workbench.screens.scenariosimulation.client.renderers.ScenarioGridColumnRenderer;
 import org.drools.workbench.screens.scenariosimulation.client.widgets.ScenarioGridColumn;
 import org.drools.workbench.screens.scenariosimulation.client.widgets.ScenarioGridLayer;
 import org.drools.workbench.screens.scenariosimulation.client.widgets.ScenarioGridPanel;
+import org.uberfire.ext.wires.core.grids.client.model.GridColumn;
 import org.uberfire.ext.wires.core.grids.client.widget.dom.single.impl.TextBoxSingletonDOMElementFactory;
 
 public class ScenarioSimulationUtils {
 
+    private static final TriFunction<String, String, TextBoxSingletonDOMElementFactory, ScenarioHeaderMetaData> SCENARIOHEADERMETADATA_FUNCTION = ScenarioHeaderMetaData::new;
+    private static final TriFunction<String, String, TextBoxSingletonDOMElementFactory, List<GridColumn.HeaderMetaData>> SCENARIOHEADERMETADATALIST_FUNCTION = (s, s2, f) -> Arrays.asList(SCENARIOHEADERMETADATA_FUNCTION.apply(s2, "", f), SCENARIOHEADERMETADATA_FUNCTION.apply(s, s2, f));
+
+
     public static ScenarioGridColumn getScenarioGridColumn(String columnTitle, String columnGroup, ScenarioGridPanel scenarioGridPanel, ScenarioGridLayer gridLayer) {
         TextBoxSingletonDOMElementFactory factory = FactoryProvider.getHeaderHasNameTextBoxFactory(scenarioGridPanel, gridLayer);
-        return new ScenarioGridColumn(new ScenarioHeaderMetaData(columnTitle, columnGroup, factory), new ScenarioGridColumnRenderer(), 100, false);
+        return new ScenarioGridColumn(SCENARIOHEADERMETADATALIST_FUNCTION.apply(columnTitle, columnGroup, factory), new ScenarioGridColumnRenderer(), 100, false);
     }
+
+    public static ScenarioGridColumn getScenarioGridColumn(String columnTitle, ScenarioGridPanel scenarioGridPanel, ScenarioGridLayer gridLayer) {
+        TextBoxSingletonDOMElementFactory factory = FactoryProvider.getHeaderHasNameTextBoxFactory(scenarioGridPanel, gridLayer);
+        return new ScenarioGridColumn(SCENARIOHEADERMETADATA_FUNCTION.apply(columnTitle, "", factory), new ScenarioGridColumnRenderer(), 100, false);
+    }
+
 }
