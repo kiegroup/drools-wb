c0d39071d60fa38b8d09ff557ff89d5dbda57da1
Daniele Zonca <daniele.zonca@gmail.com>
2018-10-08 18:00:39 +0200

    [DROOLS-2687] Add test

--- drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-backend/src/test/java/org/drools/workbench/screens/scenariosimulation/backend/server/expression/ParameterizedBaseExpressionEvaluatorTest.java
+++ drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-backend/src/test/java/org/drools/workbench/screens/scenariosimulation/backend/server/expression/ParameterizedBaseExpressionEvaluatorTest.java
@@ -47,7 +47,8 @@ public class ParameterizedBaseExpressionEvaluatorTest {
                 {2, "[ 1, 2 ,3]"},
                 {"3", "[ 1, 2 ,3]"},
                 {4, "![ 1, 2 ,3]"},
-                {4, "! < 1"}
+                {4, "! < 1"},
+                {1, "> -1"}
         });
     }
 
