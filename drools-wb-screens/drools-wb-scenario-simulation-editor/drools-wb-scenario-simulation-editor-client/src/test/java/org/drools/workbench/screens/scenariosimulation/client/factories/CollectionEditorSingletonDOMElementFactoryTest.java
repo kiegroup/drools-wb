adb4ddcb1e457855f48d1fa35cfb41817f346b76
yesamer <yamer@redhat.com>
2019-04-12 10:38:07 +0200

    [DROOLS-3873] Tests added

--- drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/test/java/org/drools/workbench/screens/scenariosimulation/client/factories/CollectionEditorSingletonDOMElementFactoryTest.java
+++ drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/test/java/org/drools/workbench/screens/scenariosimulation/client/factories/CollectionEditorSingletonDOMElementFactoryTest.java
@@ -16,10 +16,14 @@
 
 package org.drools.workbench.screens.scenariosimulation.client.factories;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.ait.lienzo.test.LienzoMockitoTestRunner;
 import com.google.gwt.dom.client.DivElement;
 import org.drools.workbench.screens.scenariosimulation.client.collectioneditor.CollectionPresenter;
 import org.drools.workbench.screens.scenariosimulation.client.collectioneditor.CollectionViewImpl;
+import org.drools.workbench.screens.scenariosimulation.model.typedescriptor.FactModelTree;
 import org.drools.workbench.screens.scenariosimulation.utils.ScenarioSimulationSharedUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -29,7 +33,9 @@ import org.mockito.Mock;
 import static org.drools.workbench.screens.scenariosimulation.client.utils.ScenarioSimulationUtils.isSimpleJavaType;
 import static org.drools.workbench.screens.scenariosimulation.model.ScenarioSimulationModel.Type.DMN;
 import static org.drools.workbench.screens.scenariosimulation.model.ScenarioSimulationModel.Type.RULE;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -40,6 +46,9 @@ public class CollectionEditorSingletonDOMElementFactoryTest extends AbstractFact
 
     protected final String STRING_CLASS_NAME = String.class.getCanonicalName();
     protected final String NUMBER_CLASS_NAME = Number.class.getCanonicalName();
+    protected final Map<String, String> expectedMapForNotSimpleType = new HashMap<>();
+    protected final Map<String, String> expectedMapForNotSimpleType1 = new HashMap<>();
+    protected final Map<String, String> expectedMapForNotSimpleType2= new HashMap<>();
 
     protected CollectionEditorSingletonDOMElementFactory collectionEditorSingletonDOMElementFactoryMock;
     protected CollectionViewImpl collectionEditorViewImpl;
@@ -49,6 +58,16 @@ public class CollectionEditorSingletonDOMElementFactoryTest extends AbstractFact
 
     @Mock
     protected DivElement collectionEditorModalBodyMock;
+
+    @Mock
+    protected FactModelTree factModelTreeMock;
+
+    @Mock
+    protected FactModelTree factModelTreeMock1;
+
+    @Mock
+    protected FactModelTree factModelTreeMock2;
+
 
     @Before
     public void setup() {
@@ -64,22 +83,69 @@ public class CollectionEditorSingletonDOMElementFactoryTest extends AbstractFact
                                                                                                                  scenarioGridLayerMock,
                                                                                                                  scenarioGridMock,
                                                                                                                  scenarioSimulationContextLocal,
-                                                                                                                 viewsProviderMock) {
-            @Override
-            protected void manageList(CollectionViewImpl collectionEditorView, String key, String genericTypeName0) {
-                // DO nothing
-            }
-
-            @Override
-            protected void manageMap(CollectionViewImpl collectionEditorView, String key, String genericTypeName0, String genericTypeName1, boolean isRule) {
-                //Do nothing
-            }
-
-        });
-
+                                                                                                                 viewsProviderMock));
         factMappingMock.getGenericTypes().add(STRING_CLASS_NAME);
         factMappingMock.getGenericTypes().add(NUMBER_CLASS_NAME);
         when(factMappingMock.getFactAlias()).thenReturn(FULL_CLASS_NAME);
+        when(scenarioSimulationContextLocal.getDataObjectFieldsMap().get(any())).thenReturn(factModelTreeMock);
+        /* This FactModelTree is used to test setCollectionEditorStructureData() method (return null in any case) */
+        when(factModelTreeMock.getSimpleProperties()).thenReturn(new HashMap<>());
+        when(factModelTreeMock.getExpandableProperties()).thenReturn(new HashMap<>());
+        /* This FactModelTree is used to test manageList and manageMap methods*/
+        when(factModelTreeMock1.getSimpleProperties()).thenReturn(expectedMapForNotSimpleType);
+        when(factModelTreeMock1.getExpandableProperties()).thenReturn(expectedMapForNotSimpleType1);
+        when(scenarioSimulationContextLocal.getDataObjectFieldsMap().get(FULL_CLASS_NAME)).thenReturn(factModelTreeMock1);
+        expectedMapForNotSimpleType.put("x", "y");
+        expectedMapForNotSimpleType1.put("a", "b");
+        when(scenarioSimulationContextLocal.getDataObjectFieldsMap().get("testclass")).thenReturn(factModelTreeMock2);
+        when(factModelTreeMock2.getSimpleProperties()).thenReturn(expectedMapForNotSimpleType2);
+        when(factModelTreeMock2.getExpandableProperties()).thenReturn(expectedMapForNotSimpleType1);
+        expectedMapForNotSimpleType2.put("z", "w");
+    }
+
+    @Test
+    public void manageList() {
+        String key =  FULL_CLASS_NAME + "#" + LIST_CLASS_NAME;
+        collectionEditorSingletonDOMElementFactoryMock.manageList(collectionEditorViewImpl, key, STRING_CLASS_NAME);
+        verify(collectionEditorViewImpl, times(1)).setListWidget(true);
+        Map<String, String> expectedMap1 = new HashMap<>();
+        expectedMap1.put("value", STRING_CLASS_NAME);
+        verify(collectionEditorViewImpl, times(1)).initListStructure(eq(key), eq(expectedMap1), isA(Map.class));
+        verify(collectionEditorSingletonDOMElementFactoryMock, times(1)).getExpandablePropertiesMap(eq(STRING_CLASS_NAME));
+    }
+
+    @Test
+    public void manageMap_RuleSimpleType() {
+        Map<String, String> expectedMap1 = new HashMap<>();
+        expectedMap1.put("value", NUMBER_CLASS_NAME);
+        manageMap(NUMBER_CLASS_NAME, true, expectedMap1);
+
+    }
+
+    @Test
+    public void manageMap_NotRuleSimpleType() {
+        Map<String, String> expectedMap1 = new HashMap<>();
+        expectedMap1.put("value", NUMBER_CLASS_NAME);
+        manageMap(NUMBER_CLASS_NAME, false, expectedMap1);
+    }
+
+    @Test
+    public void manageMap_RuleNotSimpleType() {
+        manageMap(FULL_CLASS_NAME, true, expectedMapForNotSimpleType2);
+    }
+
+    @Test
+    public void manageMap_NotRuleNotSimpleType() {
+        manageMap(FULL_CLASS_NAME, false, expectedMapForNotSimpleType);
+    }
+
+    private void manageMap(String genericType1, boolean isRule, Map<String, String> expectedMap1) {
+        String key =  FULL_CLASS_NAME + "#" + LIST_CLASS_NAME;
+        Map<String, String> expectedMap0 = new HashMap<>();
+        expectedMap0.put("value", STRING_CLASS_NAME);
+        collectionEditorSingletonDOMElementFactoryMock.manageMap(collectionEditorViewImpl, key, STRING_CLASS_NAME, genericType1, isRule);
+        verify(collectionEditorViewImpl, times(1)).setListWidget(false);
+        verify(collectionEditorViewImpl, times(1)).initMapStructure(eq(key), eq(expectedMap0), eq(expectedMap1));
     }
 
     @Test(expected = IllegalStateException.class)
