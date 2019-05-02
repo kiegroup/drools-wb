b642c70ad38486a196ecbf2ee2742462918d80a6
Daniele Zonca <daniele.zonca@gmail.com>
2019-05-02 18:35:53 +0200

    [DROOLS-3742] Fix checkstyle

--- drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/test/java/org/drools/workbench/screens/scenariosimulation/client/AbstractScenarioSimulationTest.java
+++ drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/test/java/org/drools/workbench/screens/scenariosimulation/client/AbstractScenarioSimulationTest.java
@@ -19,7 +19,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.stream.IntStream;
