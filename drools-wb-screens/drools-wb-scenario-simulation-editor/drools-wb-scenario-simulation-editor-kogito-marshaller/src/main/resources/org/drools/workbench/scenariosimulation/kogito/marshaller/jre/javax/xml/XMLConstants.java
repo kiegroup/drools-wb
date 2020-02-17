cad4a3d5742aa648d6f316e786da220625112b09
yesamer <yamer@redhat.com>
2020-02-17 15:39:03 +0100

    DROOLS-4249: Required changes after Code Review

--- drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-kogito-marshaller/src/main/resources/org/drools/workbench/scenariosimulation/kogito/marshaller/jre/javax/xml/XMLConstants.java
+++ drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-kogito-marshaller/src/main/resources/org/drools/workbench/scenariosimulation/kogito/marshaller/jre/javax/xml/XMLConstants.java
@@ -21,7 +21,6 @@ import jsinterop.annotations.JsType;
 /**
  * GWT Super-source version of javax.xml.XMLConstants.
  */
-@SuppressWarnings("unused")
 @JsType(namespace = JsPackage.GLOBAL)
 public final class XMLConstants {
 
