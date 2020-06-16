842530c5bc3611c0c4862ce8605cbd760f7541f6
Yeser Amer <yamer@redhat.com>
2020-06-16 13:58:11 +0200

    DROOLS-3799: Checkstyle

--- drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/test/java/org/drools/workbench/screens/scenariosimulation/client/rightpanel/SettingsViewImplTest.java
+++ drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-client/src/test/java/org/drools/workbench/screens/scenariosimulation/client/rightpanel/SettingsViewImplTest.java
@@ -26,7 +26,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
-import static org.drools.workbench.screens.scenariosimulation.client.rightpanel.SettingsViewImpl.*;
+import static org.drools.workbench.screens.scenariosimulation.client.rightpanel.SettingsViewImpl.DMN_MODEL_LABEL;
+import static org.drools.workbench.screens.scenariosimulation.client.rightpanel.SettingsViewImpl.DMN_NAMESPACE_LABEL;
+import static org.drools.workbench.screens.scenariosimulation.client.rightpanel.SettingsViewImpl.DMN_NAME_LABEL;
+import static org.drools.workbench.screens.scenariosimulation.client.rightpanel.SettingsViewImpl.STATELESS_LABEL;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.spy;
