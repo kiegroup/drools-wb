f5f5993a0b5202bf90b6acb44a762611b6483d8f
rikkola <toni@rikkola.net>
2020-11-12 10:17:57 +0200

    DROOLS-5808 : XLS to GDST conversion should ensure types work
    DROOLS-5807 : GDST to XLS conversion should not convert dialect column

--- drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-backend/src/main/java/org/drools/workbench/screens/guided/dtable/backend/server/conversion/BRLColumnSubHeaderBuilderDefault.java
+++ drools-wb-screens/drools-wb-guided-dtable-editor/drools-wb-guided-dtable-editor-backend/src/main/java/org/drools/workbench/screens/guided/dtable/backend/server/conversion/BRLColumnSubHeaderBuilderDefault.java
@@ -144,7 +144,7 @@ public class BRLColumnSubHeaderBuilderDefault
         for (int i = 0; i < childColumns.size(); i++) {
             final BRLVariableColumn brlConditionVariableColumn = childColumns.get(i);
             columnContext.addBoundName(brlConditionVariableColumn.getVarName());
-            if (brlConditionVariableColumn.getVarName().isEmpty()) {
+            if (brlConditionVariableColumn.getVarName() == null || brlConditionVariableColumn.getVarName().isEmpty()) {
                 result.add(new DTCellValue52(true));
             } else {
                 String varName = brlConditionVariableColumn.getVarName();
