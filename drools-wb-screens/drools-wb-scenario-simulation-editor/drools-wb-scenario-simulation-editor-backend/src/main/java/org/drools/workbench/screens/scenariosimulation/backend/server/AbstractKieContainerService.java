f0acfc1793bf91342adbfa9190875a98d9f9fb14
rikkola <toni@rikkola.net>
2020-11-18 17:25:41 +0200

    Revert "DROOLS-5458 : Project broken after test scenario run with possibly a class-loading issue"
    This reverts commit b77157c42168b187ba7675884c64a8af214a66da.

--- drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-backend/src/main/java/org/drools/workbench/screens/scenariosimulation/backend/server/AbstractKieContainerService.java
+++ drools-wb-screens/drools-wb-scenario-simulation-editor/drools-wb-scenario-simulation-editor-backend/src/main/java/org/drools/workbench/screens/scenariosimulation/backend/server/AbstractKieContainerService.java
@@ -19,9 +19,6 @@ package org.drools.workbench.screens.scenariosimulation.backend.server;
 import javax.inject.Inject;
 
 import org.kie.api.runtime.KieContainer;
-import org.kie.workbench.common.services.backend.builder.core.Builder;
-import org.kie.workbench.common.services.backend.builder.service.BuildInfo;
-import org.kie.workbench.common.services.backend.builder.service.BuildInfoImpl;
 import org.kie.workbench.common.services.backend.builder.service.BuildInfoService;
 import org.kie.workbench.common.services.shared.project.KieModule;
 import org.kie.workbench.common.services.shared.project.KieModuleService;
@@ -35,23 +32,12 @@ public abstract class AbstractKieContainerService {
     private BuildInfoService buildInfoService;
 
     protected KieContainer getKieContainer(Path path) {
-        final KieModule kieModule = moduleService.resolveModule(path);
-
-        BuildInfo buildInfo = buildInfoService.getBuildInfo(kieModule);
-        if (buildInfo instanceof BuildInfoImpl) {
-            // The kie builder needs to be cloned so that the original does not get altered.
-            final Builder clone = ((BuildInfoImpl) buildInfo).getBuilder().clone();
-            clone.build();
-
-            final KieContainer kieContainer = clone.getKieContainer();
-            if (kieContainer == null) {
-                throw new IllegalArgumentException("Retrieving KieContainer has failed. Fix all compilation errors within the " +
-                                                           "project and build the project again.");
-            }
-            return kieContainer;
+        KieModule kieModule = moduleService.resolveModule(path);
+        KieContainer kieContainer = buildInfoService.getBuildInfo(kieModule).getKieContainer();
+        if (kieContainer == null) {
+            throw new IllegalArgumentException("Retrieving KieContainer has failed. Fix all compilation errors within the " +
+                                                       "project and build the project again.");
         }
-
-        throw new IllegalStateException("Failed to clone Builder.");
-
+        return kieContainer;
     }
 }
